3e569b587dcc4e498ff1472aec29acf2abb90a8a
obbaeiei <ob.akesiripong@gmail.com>
2017-09-13 19:59:48 +0700

    add circle and fix standardjs

--- src/controllers/board.controller.js
+++ src/controllers/board.controller.js
@@ -1,4 +1,3 @@
-const _ = require('lodash')
 const APIError = require('../helpers/APIError')
 const Board = require('../models/board.model')
 const Utility = require('../helpers/utility')
@@ -6,7 +5,7 @@ const Utility = require('../helpers/utility')
 /**
  * Load board and append to req.
  */
-function load(req, res, next, id) {
+function load (req, res, next, id) {
   Board.get(id)
     .then((board) => {
       req.board = board
@@ -19,7 +18,7 @@ function load(req, res, next, id) {
  * Get board
  * @returns {Board}
  */
-function get(req, res) {
+function get (req, res) {
   return res.json(req.board)
 }
 
@@ -29,7 +28,7 @@ function get(req, res) {
  * @property {string} req.body.mobileNumber - The mobileNumber of board.
  * @returns {Board}
  */
-function create(req, res, next) {
+function create (req, res, next) {
   const squreGrid = 10 // default grid 10x10
   const board = new Board({
     square_grid: squreGrid
@@ -46,7 +45,7 @@ function create(req, res, next) {
  * @property {string} req.body.mobileNumber - The mobileNumber of board.
  * @returns {Board}
  */
-function update(req, res, next) {
+function update (req, res, next) {
   const board = req.board
   board.boardname = req.body.boardname
   board.mobileNumber = req.body.mobileNumber
@@ -62,7 +61,7 @@ function update(req, res, next) {
  * @property {number} req.query.limit - Limit number of boards to be returned.
  * @returns {Board[]}
  */
-function list(req, res, next) {
+function list (req, res, next) {
   const { limit = 50, skip = 0 } = req.query
   Board.list({
     limit,
@@ -76,7 +75,7 @@ function list(req, res, next) {
  * Delete board.
  * @returns {Board}
  */
-function remove(req, res, next) {
+function remove (req, res, next) {
   const board = req.board
   board.remove()
     .then(deletedBoard => res.json(deletedBoard))
@@ -87,7 +86,7 @@ function remove(req, res, next) {
  * add unit
  * @returns {Board}
  */
-async function addUnit(req, res, next) {
+async function addUnit (req, res, next) {
   // @DONE @TODO get ship from type
   // @DONE @TODO check type ship should not more than 1x Battleship, 2x Cruisers, 3x Destroyers and 4x Submarines.
   // @DONE @TODO get all cors cells from start and l and direction
@@ -97,16 +96,16 @@ async function addUnit(req, res, next) {
   // @DONE @TODO return success
   const board = req.board
   const body = req.body
-  console.log('body = ', JSON.stringify(body, null, 2));
+  console.log('body = ', JSON.stringify(body, null, 2))
 
   // get ship from type
   const ship = Utility.getShip(body.type)
-  console.log('ship = ', JSON.stringify(ship, null, 2));
-  console.log('board = ', JSON.stringify(board, null, 2));
+  console.log('ship = ', JSON.stringify(ship, null, 2))
+  console.log('board = ', JSON.stringify(board, null, 2))
 
   // check type ship should not more than 1x Battleship, 2x Cruisers, 3x Destroyers and 4x Submarines.
   const isNotLimitExeeded = Utility.checkLimitShipsInBoard(board.ships, ship.type)
-  console.log('isNotLimitExeeded = ', isNotLimitExeeded);
+  console.log('isNotLimitExeeded = ', isNotLimitExeeded)
   if (!isNotLimitExeeded) {
     res.json('illegal')
     return
@@ -114,14 +113,14 @@ async function addUnit(req, res, next) {
 
   // get all cors cells from start and l and direction
   const cors = Utility.getCors(body, ship)
-  console.log('cors = ', JSON.stringify(cors, null, 2));
+  console.log('cors = ', JSON.stringify(cors, null, 2))
   ship.cors = cors // set cors to ship
-  console.log('board = ', JSON.stringify(board, null, 2));
-  console.log('ship = ', JSON.stringify(ship, null, 2));
+  console.log('board = ', JSON.stringify(board, null, 2))
+  console.log('ship = ', JSON.stringify(ship, null, 2))
 
   // check ship over grid
   const isOver = Utility.isShipOverGrid(board.square_grid, ship.cors)
-  console.log('isOver = ', isOver);
+  console.log('isOver = ', isOver)
   if (isOver) {
     res.json('illegal')
     return
@@ -129,7 +128,7 @@ async function addUnit(req, res, next) {
 
   // check ship exists or adjacent cells
   const isAdjacent = Utility.isAdjacent(board.ships, ship.cors)
-  console.log('isAdjacent = ', isAdjacent);
+  console.log('isAdjacent = ', isAdjacent)
   if (isAdjacent) {
     res.json('illegal')
     return
@@ -137,7 +136,8 @@ async function addUnit(req, res, next) {
 
   // pass all cases
   board.ships.push(ship)
-  console.log('board = ', JSON.stringify(board, null, 2));
+  console.log('board = ', JSON.stringify(board, null, 2))
+  // @TODO update state if all of ships are place
 
   try {
     await board.save()
@@ -150,20 +150,21 @@ async function addUnit(req, res, next) {
 
 /**
  * Attacker fire the ship.
- * @returns {Promise<String, APIError>} text - 
+ * @returns {Promise<String, APIError>} text -
  */
-async function fire(req, res, next) {
+async function fire (req, res, next) {
+  // @TODO check if state not ready (put more ships please) cannot fire
   const board = req.board
   const x = req.body.x
   const y = req.body.y
   const cell = `${x}x${y}`
   board.fired.push(cell)
-  //console.log('board = ', JSON.stringify(board, null, 2));
+  // console.log('board = ', JSON.stringify(board, null, 2));
 
   // Get result and set field in board object
   const result = Utility.fireAndGetSituation(board, cell)
 
-  console.log('board = ', JSON.stringify(board, null, 2));
+  console.log('board = ', JSON.stringify(board, null, 2))
   try {
     board.markModified('ships') // update mixed type in mongoose
     await board.save()
